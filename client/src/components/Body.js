a9d115a67408c7f368913254f1d19cb24c06b9ed
Han Wang <hyw2@cornell.edu>
2020-03-23 02:49:50 -0400

    Add card template with bootstrap

--- client/src/components/Body.js
+++ client/src/components/Body.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import Nav from './Nav';
 
 export class Body extends Component {
     render() {
         return (
-            <div style={bodyStyle}>
-                <Nav />
+            <div className="container-fluid pt-2">
+                <div className="card">
+                <div className="card-header">
+                    <ul className="nav nav-tabs card-header-tabs">
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Application</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Resume</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Scores</a>
+                    </li>
+                    </ul>
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Special title treatment</h5>
+                    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                < div className = "card-footer"
+                style = {{width: '100%', height: '100%', backgroundColor: '#32DE8A', color: 'white'}}> Review Resume </div>
+                </div>
             </div>
         )
     }
-}
-
-const bodyStyle = {
-    padding: '20px 30px'
 }
 
 export default Body
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+// Import Bootstrap
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import index CSS to override Bootstrap
+import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
