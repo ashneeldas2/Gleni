276935473acb251f1f9515ac0dbd72582c1f8bb1
Han Wang <hyw2@cornell.edu>
2020-03-26 04:23:31 -0400

    Add resume and interactivity

---
+++ client/src/components/Resume.js
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react'
+
+export default class Resume extends PureComponent {
+    render() {
+        return (
+            <div>
+                <embed src="https://drive.google.com/file/d/1hmV29kInLSPJNGbnniRpmHvRxvSSgsQP/preview"  width="100%" height="580"/>
+            </div>
+        )
+    }
+}
